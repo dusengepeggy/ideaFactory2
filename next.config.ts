dd68756c7a997bbd2e406924a9796b2c53f47d7b
dusengepeggy <dickinepeggy100@gmail.com>
2024-11-06 17:33:52 +0200

    linking pictures

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["t3.ftcdn.net"],
+    domains: ["t3.ftcdn.net","cdn.stocksnap.io","static.boredpanda.com","assets.weforum.org","img.freepik.com","images.unsplash.com","i.pinimg.com"],
   },
 };
 
